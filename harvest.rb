532b1ce8c27acb2b31c99c6d8efe714d2f46e89e
Jakob Buis <jpabuis@gmail.com>
2014-02-24 11:55:55 +0100

    basic class for github api

--- harvest.rb
+++ harvest.rb
@@ -1,6 +1,27 @@
 # Gather all repositories for Eclipse
-# Get all contributors
-# For each contributor
-    # get all repositories
-    # store in database
-# end
+require 'httparty'
+require 'config.rb'
+
+# Basic class for consuming the API
+class GitHub
+    include HTTParty
+    base_uri 'https://api.github.com'
+    basic_auth @username, @password
+
+    def initialize(user, pass)
+        @username = user
+        @password = pass
+    end
+
+    # Gather all repositories from GitHub
+    def execute!
+        # Get all contributors
+        # For each contributor
+            # get all repositories
+            # store in database
+        # end
+    end
+end
+
+# Execute the class
+GitHub.new(config.github.username, config.github.password).execute!
